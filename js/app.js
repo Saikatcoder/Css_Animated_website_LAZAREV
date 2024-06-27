79942415ab178690301da653a0301226a5fdf12f
Saikat Dutta <saikat2018dutta18@gmail.com>
2024-06-27 19:38:57 +0530

    message

---
+++ js/app.js
@@ -0,0 +1,43 @@
+function navAnimation() {
+    let nav = document.querySelector(".nav")
+
+    nav.addEventListener("mouseenter", function () {
+        let tl = gsap.timeline()
+
+        tl.to(".nav-bottom", {
+            height: "21vh",
+            duration: 0.5
+        })
+        tl.to(".nav-content h5", {
+            display: "block",
+            duration: 0.1
+
+        })
+        tl.to(".nav-content h5 span", {
+            y: 0,
+            stagger: {
+                amount: 0.5
+            }
+        })
+    })
+    nav.addEventListener("mouseleave", function () {
+        let tl = gsap.timeline()
+        tl.to(".nav-content h5 span", {
+            y: 25,
+            stagger: {
+                amount: 0.2
+            }
+        })
+        tl.to(".nav-content h5", {
+            display: "none",
+            duration: 0.1
+        })
+        tl.to(".nav-bottom", {
+            height: 0,
+            duration: 0.3
+        })
+    })
+}
+
+
+navAnimation()
